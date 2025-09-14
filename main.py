3d7736a9ce46ab458c392ef11874faa7718a1e26
Vitja72 <viktor.stefan@tn.techstarter.de>
2025-09-14 13:45:59 +0200

    Created a /post endpoint for creating an employee.

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
-from fastapi import FastAPI, Depends, HTTPException, ststus
+from fastapi import FastAPI, Depends, HTTPException, status
 from sqlalchemy.orm import Session
+from typing import List 
 from database import get_db
-app = FastAPI()
+from models import Employee, EmployeeCreate, EmployeeUpdate
+import crud
+
+app = FastAPI(
+    title = "Employee Management API",
+    description = "Adding, reading, updating and deleting employees from Postgres DB.",
+    version = "1.0"
+)
 
 @app.get("/health")
 def health_check():
     return {"status":"all good and dandy"}
+
+@app.post("/employees", response_model=Employee, status_code=status.HTTP_201_CREATED)
+def created_employee_endpoint(employee: EmployeeCreate, db: Session = Depends(get_db)):
+    try:
+        db_employee = crud.create_employee(db = db, employee = employee)
+        return db_employee 
+    
+    except ValueError as e:
+        raise HTTPException(status_code=400, details=str(e))
+
+
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host = "0.0.0.0", port = 8000)
